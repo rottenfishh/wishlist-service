9a97697c36a4673e246129eb038570f205cad682
Алина Колодина <a.kolodina@g.nsu.ru>
2026-04-13 14:57:45 +0700

    feat: wishlist repo and service

---
+++ internal/adapter/out/repository/wishlist.go
@@ -0,0 +1,137 @@
+package repository
+
+import (
+	"cdek/internal/model"
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+type WishlistRepository struct {
+	db *sql.DB
+}
+
+func (r *WishlistRepository) Save(ctx context.Context, wishlist *model.Wishlist) (*model.Wishlist, error) {
+	query := `INSERT INTO wishlists(user_id, token, title, description, date) 
+              VALUES ($1, $2, $3, $4, $5)
+              RETURNING id, user_id, token, title, description, date;`
+
+	row := r.db.QueryRowContext(ctx, query, wishlist.UserID, wishlist.Token,
+		wishlist.Title, wishlist.Description, wishlist.Date)
+
+	var savedWishlist model.Wishlist
+
+	err := row.Scan(&savedWishlist.ID, &savedWishlist.UserID, &savedWishlist.Token,
+		&savedWishlist.Title, &savedWishlist.Description, &savedWishlist.Date)
+	if err != nil {
+		return nil, fmt.Errorf("save wishlist: %w", err)
+	}
+
+	return &savedWishlist, nil
+}
+
+func (r *WishlistRepository) Update(ctx context.Context, wishlist *model.Wishlist) (*model.Wishlist, error) {
+	query := `UPDATE wishlists SET title = $1, description = $2, date = $3
+              WHERE id = $4;`
+
+	row := r.db.QueryRowContext(ctx, query, wishlist.Title, wishlist.Description, wishlist.Date, wishlist.ID)
+
+	var updatedWishlist model.Wishlist
+
+	err := row.Scan(&updatedWishlist.ID, &updatedWishlist.UserID, &updatedWishlist.Token,
+		&updatedWishlist.Title, &updatedWishlist.Description, &updatedWishlist.Date)
+	if err != nil {
+		return nil, fmt.Errorf("update wishlist: %w", err)
+	}
+
+	return &updatedWishlist, nil
+}
+
+func (r *WishlistRepository) Delete(ctx context.Context, ID int64) (*model.Wishlist, error) {
+	query := `DELETE FROM wishlists WHERE id = $1
+              RETURNING id, user_id, token, title, description, date;`
+
+	row := r.db.QueryRowContext(ctx, query, ID)
+
+	var wishlist model.Wishlist
+
+	err := row.Scan(&wishlist.ID, &wishlist.UserID, &wishlist.Token,
+		&wishlist.Title, &wishlist.Description, &wishlist.Date)
+	if err != nil {
+		return nil, fmt.Errorf("delete wishlist: %w", err)
+	}
+
+	return &wishlist, nil
+
+}
+
+func (r *WishlistRepository) GetByID(ctx context.Context, ID int64) (*model.Wishlist, error) {
+	query := `SELECT id, user_id, token, title, description, date
+              FROM wishlists 
+              WHERE id = $1;`
+
+	row := r.db.QueryRowContext(ctx, query, ID)
+
+	var wishlist model.Wishlist
+
+	err := row.Scan(&wishlist.ID, &wishlist.UserID, &wishlist.Token,
+		&wishlist.Title, &wishlist.Description, &wishlist.Date)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, model.NotFound
+		}
+		return nil, fmt.Errorf("get by id wishlist: %w", err)
+	}
+
+	return &wishlist, nil
+}
+
+func (r *WishlistRepository) GetByUserID(ctx context.Context, userID uuid.UUID) ([]model.Wishlist, error) {
+	query := `SELECT id, user_id, token, title, description, date
+              FROM wishlists
+              WHERE user_id = $1;`
+
+	rows, err := r.db.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, fmt.Errorf("get by user id wishlist: %w", err)
+	}
+	defer rows.Close()
+
+	var wishlists []model.Wishlist
+	for rows.Next() {
+		var wishlist model.Wishlist
+		err = rows.Scan(&wishlist.ID, &wishlist.UserID, &wishlist.Token, &wishlist.Title,
+			&wishlist.Description, &wishlist.Date)
+		if err != nil {
+			return nil, fmt.Errorf("get by user id wishlist: %w", err)
+		}
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("get by user id wishlists: %w", err)
+	}
+
+	return wishlists, nil
+}
+
+func (r *WishlistRepository) GetByToken(ctx context.Context, token uuid.UUID) (*model.Wishlist, error) {
+	query := `SELECT id, user_id, token, title, description, date
+              FROM wishlists
+              WHERE token = $1;`
+	row := r.db.QueryRowContext(ctx, query, token)
+
+	var wishlist model.Wishlist
+	err := row.Scan(&wishlist.ID, &wishlist.UserID, &wishlist.Token,
+		&wishlist.Title, &wishlist.Description, &wishlist.Date)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, model.NotFound
+		}
+	}
+
+	return &wishlist, nil
+}
---
+++ internal/service/wishlist/service.go
@@ -0,0 +1,74 @@
+package wishlist
+
+import (
+	"cdek/internal/model"
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) Service {
+	return &service{repo}
+}
+
+func (s *service) Create(ctx context.Context, userID uuid.UUID, name, description string,
+	date time.Time) (*model.Wishlist, error) {
+
+	wishlist := &model.Wishlist{
+		UserID:      userID,
+		Token:       uuid.New(),
+		Title:       name,
+		Description: description,
+		Date:        date,
+	}
+
+	return s.repo.Save(ctx, wishlist)
+}
+
+func (s *service) Update(ctx context.Context, userID uuid.UUID, wishlistID int64, name,
+	description string, date time.Time) (*model.Wishlist, error) {
+
+	wishlist, err := s.repo.GetByID(ctx, wishlistID)
+	if err != nil {
+		return nil, err
+	}
+
+	if wishlist.UserID != userID {
+		return nil, model.Forbidden
+	}
+
+	wishlist.Title = name
+	wishlist.Description = description
+	wishlist.Date = date
+	return s.repo.Update(ctx, wishlist)
+}
+
+func (s *service) GetByUserID(ctx context.Context, userID uuid.UUID) ([]model.Wishlist, error) {
+	return s.repo.GetByUserID(ctx, userID)
+}
+
+func (s *service) GetByID(ctx context.Context, id int64) (*model.Wishlist, error) {
+	return s.repo.GetByID(ctx, id)
+}
+
+func (s *service) GetByToken(ctx context.Context, token uuid.UUID) (*model.Wishlist, error) {
+	return s.repo.GetByToken(ctx, token)
+}
+
+func (s *service) Delete(ctx context.Context, userID uuid.UUID, wishlistID int64) (*model.Wishlist, error) {
+	wishlist, err := s.repo.GetByID(ctx, wishlistID)
+	if err != nil {
+		return nil, err
+	}
+
+	if wishlist.UserID != userID {
+		return nil, model.Forbidden
+	}
+
+	return s.repo.Delete(ctx, wishlistID)
+}
